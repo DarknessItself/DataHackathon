7e1b71c72466127cf624c8b9f4cecbdf65be3a44
Daniel Doucett <doucettdaniel@gmail.com>
2019-11-17 00:10:52 -0400

    Flagging complete

--- GeoLocations.py
+++ GeoLocations.py
@@ -16,17 +16,30 @@ types = ["accounting", "airport", "amusement_park", "aquarium", "art_gallery", "
 
 
 isInteresting = False
+isFlagged = False
+isPostOffice = False
+isEmbassy = False
 
-parser = argparse.ArgumentParser(description="Parse the thing")
-parser.add_argument('stringname', metavar='N', type=str, nargs='+', 
+parser = argparse.ArgumentParser(
+	description="Using an address in natural language find out more detailed"
+	" info including nearby businesses")
+parser.add_argument('stringname', metavar='address', type=str, nargs='+',
 	help="Address for the building")
-parser.add_argument('--fb', dest='printPost', help="Find the building type",
+parser.add_argument('--pld', dest='printLocalData', help="Find the building type",
+	action="store_true")
+parser.add_argument('--ld', dest='localData', 
+	help="Uses places data to find"
+		 " what business are close to the location.",
+    action="store_true")
+parser.add_argument("--pad", dest='printAddressData',
+	help="Print only the building info for building with the same "
+	     "address as the input. Overrides --pld",
 	action="store_true")
 
 args=parser.parse_args()
 
 
-#
+#Variables for APIs
 url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 urlplaces = 'https://maps.googleapis.com/maps/api/place/details/json?'
 key = '&key=AIzaSyCgcV2R4KkxhqdnzXXMAbYA4VLEBQd7w-8'
@@ -60,53 +73,155 @@ def search_places_by_coordinate(location, radius):
     
     return places
 
-address = args.stringname[0]
-print(address)
-addressSendable = address.replace(" ", "+")
+#Method to get the data for the location
+def get_street_info():
+	address = args.stringname[0]
+	print(address)
+	addressSendable = address.replace(" ", "+")
 
-query = url + addressSendable + key
-page = requests.get(query)
-geoData = page.json()
+	query = url + addressSendable + key
+	page = requests.get(query)
+	geoData = page.json()
 
-try:
-	place_id = geoData["results"][0]['place_id']
-except KeyError:
-	print("Failed")
-	place_id = ""
+	lattitude = geoData['results'][0]['geometry']['location']['lat']
+	longitude = geoData['results'][0]['geometry']['location']['lng']
+	addressInfo = geoData['results'][0]['address_components']
 
-print(place_id)
+	#print("\nBens Code\n")
+	'''
+	for i in geoData['results'][0]['address_components']:
+		#if (i['types'][0] != "route" and i['types'][0] != "street_number"):
+		if i['short_name'] not in addressInfo:
+			addressInfo.append(i['short_name'])
+	'''
 
-lattitude = geoData['results'][0]['geometry']['location']['lat']
-longitude = geoData['results'][0]['geometry']['location']['lng']
-addressInfo = []
+	print("Lattitude: ", lattitude)
+	print("Longitude: ", longitude)
 
-if place_id != "" and args.printPost:
-	places = search_places_by_coordinate(
-		",".join([str(lattitude), str(longitude)]), "50")
-	
+	try:
+		place_id = geoData["results"][0]['place_id']
+	except KeyError:
+		print("Failed")
+		place_id = ""
+
+	return {"lat": lattitude, "long": longitude, 
+		'id': place_id, 'addressInfo': addressInfo}
+
+
+def get_nearby_data(info):
+	return search_places_by_coordinate(
+		",".join([str(addressData['lat']), str(addressData['long'])]), "50")
+
+def print_place_data(data):
 	count = 0
-	for x in places:				
+
+	print("\nNearby business locations:\n")
+	for x in data:				
 		count += 1
 		try:
-			print("%2d: Name: %s, Address %s"%(count, x['name'], x['vicinity']))
+			print("%2d: Name     | %s, \n"
+				   "    Address  | %s, \n"
+				   "    Type     | %s  \n"
+				   %(count, x['name'], x['vicinity'], x['types']))
 		except KeyError:
-			print("Name: %s, No address found"% x['name'])
+			print("%2d: Name     | %s, \n" 
+				  "     Address  | No address found, \n"
+				  "     Type     | %s  \n"% (count, x['name'],x['types']))
+	print()
+
+def print_location_data(data, short):
+	print("\nAddress data for this address is:\n")
+	for i in data['addressInfo']:
+		print(" %s"%( i['short_name'] if short else i['long_name']))
 
 
-print("\nBens Code\n")
-for i in geoData['results'][0]['address_components']:
-	#if (i['types'][0] != "route" and i['types'][0] != "street_number"):
-	if i['long_name'] not in addressInfo:
-		addressInfo.append(i['long_name'])
+def get_compare_address(data):
+	x = [i['long_name'] for i in data if i['types'][0] == "street_number" or i['types'][0] == "route" or i['types'][0] == "locality"]
+	retVal = x[0] + " " + x[1] + ', ' + x[2]
+	print(retVal)
+	return retVal
 
-print("Lattitude: ", lattitude)
-print("Longitude: ", longitude)
-for i in addressInfo:
-	print(i)
+def get_businesses_with_same_address(aData, pData):
+	retVal = []
+	for i in pData:
+		try:
+			if i['vicinity'] == aData:
+				retVal.append(i)
+		except KeyError:
+			pass
+	return retVal
+
+def check_for_flaggable(data):
+	global isFlagged, isPostOffice, isEmbassy
+	flaggable = [
+		'post_office', 'car_dealer', 'cemetery', 
+		'courthouse', 'embassy', 'park', 'storage']
+
+	for i in data:
+		for x in i['types']:
+			if x in flaggable:
+				isFlagged = True
+				if x == 'post_office':
+					isPostOffice = True
+				if x == 'embassy':
+					isEmbassy = True
 
 
-if isInteresting:
+def print_address_data(aData, pData):
+	global isInteresting
+	data = get_businesses_with_same_address(aData, pData)
+	if len(data) > 0: 
+		isInteresting = True
+		check_for_flaggable(data)
+
+	print_place_data(data)
+
+	
+
+#Runtime code
+addressData = get_street_info()
+
+print_location_data(addressData, False)	
+addressTest = get_compare_address(addressData['addressInfo'])
+
+x = [i['long_name'] for i in addressData['addressInfo'] if i['types'][0] == 'postal_code']
+
+print(x)
+
+
+if args.localData:
+	placeData = get_nearby_data(addressData)
+	if args.printLocalData or args.printAddressData:
+		if args.printAddressData:
+			print_address_data(addressTest, placeData)
+		else:
+			print_place_data(placeData)
+
+
+if isFlagged:
 	print(
-		"============================================"
-		"            INTERESTING ADDRESS             "
-		"============================================")
+	"     ||====================================================||\n"	
+	"     ||         _       _     _                            ||\n"
+	"     ||        /_\\   __| | __| | _ _  ___  ___ ___         ||\n"
+	"     ||       / _ \\ / _` |/ _` || '_|/ -_)(_-<(_-<         ||\n"
+	"     ||      /_/ \\_\\\\__,_|\\__,_||_|  \\___|/__//__/         ||\n"
+	"     ||       ___  _                            _          ||\n"
+	"     ||      | __|| | __ _  __ _  __ _  ___  __| |         ||\n"
+	"     ||      | _| | |/ _` |/ _` |/ _` |/ -_)/ _` |         ||\n"
+	"     ||      |_|  |_|\\__,_|\\__, |\\__, |\\___|\\__,_|         ||\n"
+	"     ||                    |___/ |___/                     ||\n"
+	"     ||====================================================||\n"
+	)
+	
+	if isPostOffice:
+		print("\n This address is a post office. Verify not a PO box.\n")
+	
+	if isEmbassy:
+		print("\n This address is registered as an Embassy. Verify it is "
+			  "also residential space.")
+
+elif isInteresting:
+	print(
+		"==================================================================\n"
+		"          Address May Be Mixed Residential and Commercial         \n"
+		"==================================================================\n")
